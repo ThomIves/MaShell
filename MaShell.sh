8624ddbfddd8586e24dd2803ac6feabcbcbac46e
Thom Ives <thom.ives@gmail.com>
2017-09-26 08:16:01 -0600

    now checks 1st for installs

--- MaShell.sh
+++ MaShell.sh
@@ -5,21 +5,24 @@ echo 'UnumDeum73!' | sudo -S apt-get update
 echo "################ Update Complete #######################################"
 echo
 echo "################ Removed unneeded packages #############################"
-printf 'y\n' | sudo apt autoremove
+sudo apt-get -y autoremove
 echo "################ Unneeded packages removed #############################"
 echo
 echo "################ Starting Install Essentials ###########################"
-printf 'y\n' | sudo apt-get install python-pip python-dev build-essential
+dpkg -s python-pip 2>/dev/null >/dev/null || sudo apt-get -y install python-pip
+dpkg -s python-dev 2>/dev/null >/dev/null || sudo apt-get -y install python-dev
+dpkg -s build-essential 2>/dev/null >/dev/null || sudo apt-get -y install build-essential
 pip install --user -U setuptools
 pip install --user --upgrade pip
 echo "################ Essentials Install Complete ###########################"
 echo 
 echo "################ Installing Sublime ####################################"
-sudo apt-get install sublime-text
+dpkg -s sublime-text 2>/dev/null >/dev/null || sudo apt-get -y install sublime-text
 echo "################ Sublime Install Complete ##############################"
 echo 
 echo "################ Installing Dropbox ####################################"
-sudo -S apt-get install dropbox nemo-dropbox
+dpkg -s dropbox 2>/dev/null >/dev/null || sudo apt-get -y install dropbox
+dpkg -s nemo-dropbox 2>/dev/null >/dev/null || sudo apt-get -y install nemo-dropbox
 echo "################ Dropbox Install Complete ##############################"
 echo 
 echo "### Installing Python VirtualEnv Stuff #################################"
