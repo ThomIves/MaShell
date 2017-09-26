d1334e6325b00c18d99541556ff43a7fa254cbbf
Thom Ives <thom.ives@gmail.com>
2017-09-26 11:40:56 -0600

    changed the way virtualenv and pip are installed

--- MaShell.sh
+++ MaShell.sh
@@ -9,11 +9,8 @@ sudo apt-get -y autoremove
 echo "################ Unneeded packages removed #############################"
 echo
 echo "################ Starting Install Essentials ###########################"
-dpkg -s python-pip 2>/dev/null >/dev/null || sudo apt-get -y install python-pip
 dpkg -s python-dev 2>/dev/null >/dev/null || sudo apt-get -y install python-dev
 dpkg -s build-essential 2>/dev/null >/dev/null || sudo apt-get -y install build-essential
-pip install --user -U setuptools
-pip install --user --upgrade pip
 echo "################ Essentials Install Complete ###########################"
 echo 
 echo "################ Installing Sublime ####################################"
@@ -26,15 +23,16 @@ dpkg -s nemo-dropbox 2>/dev/null >/dev/null || sudo apt-get -y install nemo-drop
 echo "################ Dropbox Install Complete ##############################"
 echo 
 echo "### Installing Python VirtualEnv Stuff #################################"
-pip install --user virtualenv virtualenvwrapper
+dpkg -s python-virtualenv 2>/dev/null >/dev/null || sudo apt-get -y install python-virtualenv
+dpkg -s virtualenvwrapper 2>/dev/null >/dev/null || sudo apt-get -y install virtualenvwrapper
 if grep -q "virtualenvwrapper.sh" "/home/thom/.bashrc"; then  
     echo "virtualenvwrapper is set up properly in .bashrc"
 else  
     echo "Setting up bashrc to work with virtualenvwrapper."
-    echo mkdir /home/thom/.virtualenvs
+    mkdir /home/thom/.virtualenvs
     echo "" >> /home/thom/.bashrc
     echo "# Path to the virtualenvwrapper shell script" >> /home/thom/.bashrc
-    echo "source /home/thom/.local/bin/virtualenvwrapper.sh" >> /home/thom/.bashrc
+    echo "source /usr/share/virtualenvwrapper/virtualenvwrapper.sh" >> /home/thom/.bashrc
 fi
 echo "### Python Virtual Environment Stuff Installs Complete #################"
 echo 
